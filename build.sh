43ef5afb1b73022645d52855b0f3ac7a5b3517c1
Xmister <xmisterhu@gmail.com>
2013-04-08 00:46:02 +0200

    It's more logical this way
    Signed-off-by: Xmister <xmisterhu@gmail.com>

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ KERNEL_DIR=kernel
 KERNEL_PATH=$KBE_PATH/$KERNEL_DIR
 TOOLCHAIN=arm-linux-gnueabi-
 BUILD_DIR=build
-BUILD_LOG=build.log
+BUILD_LOG=../build.log
 cd $KBE_PATH
 rm $BUILD_LOG
 cd $KERNEL_PATH
 #Sense
 make clean 2>&1
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN clean >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN clean >> $BUILD_LOG 2>&1
 cat arch/arm/configs/ap33_android_defconfig arch/arm/configs/ap33_android_sense > .config
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j6 >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j6 >> $BUILD_LOG 2>&1
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` clean
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j2 >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j2 >> $BUILD_LOG 2>&1
 cd ..
 rm -rf build/*
 cp $KERNEL_DIR/arch/arm/boot/zImage $BUILD_DIR/zImage_sense
@@ -34,11 +34,11 @@ ver=$(cat .version)
 ver=$(($ver-1))
 echo $ver > .version
 make clean 2>&1
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN clean >> ../$BUILD_LOG 2>&1
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN ap33_android_defconfig >> ../$BUILD_LOG 2>&1
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j6 >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN clean >> $BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN ap33_android_defconfig >> $BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j6 >> $BUILD_LOG 2>&1
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` clean
-make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j2 >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j2 >> $BUILD_LOG 2>&1
 cd ..
 cp $KERNEL_DIR/arch/arm/boot/zImage $BUILD_DIR/zImage_aosp
 #AOSP end
