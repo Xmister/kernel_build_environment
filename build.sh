0b7e2cdd6b768a7666fe71f35fe124d9b32a557b
Xmister <xmisterhu@gmail.com>
2013-04-07 02:09:19 +0200

    Build Wi-Fi from compat_wireless again
    Signed-off-by: Xmister <xmisterhu@gmail.com>

--- build.sh
+++ build.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
-cd /home/xmister/htc/
-rm build.log
-cd /home/xmister/htc/kernel
+KBE_PATH=/home/xmister/htc/
+KERNEL_DIR=kernel
+KERNEL_PATH=$KBE_PATH/$KERNEL_DIR
+TOOLCHAIN=arm-linux-gnueabi-
+BUILD_DIR=build
+BUILD_LOG=build.log
+cd $KBE_PATH
+rm $BUILD_LOG
+cd $KERNEL_PATH
 #Sense
 make clean 2>&1
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- clean >> ../build.log 2>&1
-#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- ap33_android_defconfig >> ../build.log 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN clean >> ../$BUILD_LOG 2>&1
 cat arch/arm/configs/ap33_android_defconfig arch/arm/configs/ap33_android_sense > .config
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j6 >> ../build.log 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j6 >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j2
 cd ..
 rm -rf build/*
-cp kernel/arch/arm/boot/zImage build/zImage_sense
-MOD_DIR=build/sense/system/lib/modules
+cp $KERNEL_DIR/arch/arm/boot/zImage $BUILD_DIR/zImage_sense
+MOD_DIR=$BUILD_DIR/sense/system/lib/modules
 mkdir -p $MOD_DIR
-find kernel/arch -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/crypto -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/drivers -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/fs -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/ipc -type f -name '*.ko' -exec cp -f {} $MOD_DIR \; 
-find kernel/net -type f -name '*.ko' -exec cp -f {} $MOD_DIR \; 
+find $KERNEL_DIR/arch -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/crypto -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/fs -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/ipc -type f -name '*.ko' -exec cp -f {} $MOD_DIR \; 
+find $KERNEL_DIR/net -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/drivers -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/drivers/net/wireless/compat-wireless_R5.SP2.03 -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
 #Sense end
 #AOSP
-cd /home/xmister/htc/kernel
+cd $KERNEL_PATH
 ver=$(cat .version)
 ver=$(($ver-1))
 echo $ver > .version
 make clean 2>&1
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- clean >> ../build.log 2>&1
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- ap33_android_defconfig >> ../build.log 2>&1
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- -j6 >> ../build.log 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN clean >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN ap33_android_defconfig >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j6 >> ../$BUILD_LOG 2>&1
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -C drivers/net/wireless/compat-wireless_R5.SP2.03 KLIB=`pwd` KLIB_BUILD=`pwd` -j2
 cd ..
-cp kernel/arch/arm/boot/zImage build/zImage_aosp
+cp $KERNEL_DIR/arch/arm/boot/zImage $BUILD_DIR/zImage_aosp
 #AOSP end
-MOD_DIR=build/aosp/system/lib/modules
+MOD_DIR=$BUILD_DIR/aosp/system/lib/modules
 mkdir -p $MOD_DIR
-find kernel/arch -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/crypto -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/drivers -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/fs -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
-find kernel/ipc -type f -name '*.ko' -exec cp -f {} $MOD_DIR \; 
-find kernel/net -type f -name '*.ko' -exec cp -f {} $MOD_DIR \; 
+find $KERNEL_DIR/arch -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/crypto -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/fs -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/ipc -type f -name '*.ko' -exec cp -f {} $MOD_DIR \; 
+find $KERNEL_DIR/net -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/drivers -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
+find $KERNEL_DIR/drivers/net/wireless/compat-wireless_R5.SP2.03 -type f -name '*.ko' -exec cp -f {} $MOD_DIR \;
